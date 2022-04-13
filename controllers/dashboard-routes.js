336789c2647f4044a5bc59380529c6911c1ac610
Kaitlyn Skinner <kskinnerr@hotmail.com>
2022-04-13 17:19:52 -0400

    minor change it dashboard-routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -45,7 +45,7 @@ router.get('/', withAuth, (req, res) => {
 });
 
 router.get('/edit/:id', withAuth, (req, res) => {
-    Post.findByPk(req.params.id, {
+    Post.findOne(req.params.id, {
         attributes: [
             'id',
             'post_url',
